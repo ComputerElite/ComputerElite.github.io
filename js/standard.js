45b3c28508b805095b884cb32b802bb07c197957
ComputerElite <230407KH5234@ohg-landau.info>
2021-06-06 17:08:30 +0200

    add context menu

--- js/standard.js
+++ js/standard.js
@@ -36,6 +36,26 @@ function MakeJSONGetRequestAsync(url) {
     });
 }
 
+function mouseX(evt) {
+    if (evt.pageX) {
+        return evt.pageX;
+    } else if (evt.clientX) {
+        return evt.clientX + (document.documentElement.scrollLeft ? document.documentElement.scrollLeft : document.body.scrollLeft);
+    } else {
+        return null;
+    }
+}
+  
+function mouseY(evt) {
+    if (evt.pageY) {
+        return evt.pageY;
+    } else if (evt.clientY) {
+        return evt.clientY + (document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop);
+    } else {
+        return null;
+    }
+}
+
 function InIframe () {
     var inFrame = false;
     try {
