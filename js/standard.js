787b3ea1e22518853fc5236971e787636d3b9c82
ComputerElite <230407KH5234@ohg-landau.info>
2021-07-02 15:15:21 +0200

    should now be working again

--- js/standard.js
+++ js/standard.js
@@ -26,6 +26,28 @@ function MakeTextGetRequestAsync(url) {
     })
 }
 
+function GetCookie(cookieName) {
+    var name = cookieName + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function SetCookie(name, value, expiration) {
+    var d = new Date();
+    d.setTime(d.getTime() + (expiration * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = name + "=" + value + ";" + expires + ";path=/";
+}
+
 function MakeJSONGetRequestAsync(url) {
     return new Promise((resolve, reject) => {
         MakeTextGetRequestAsync(url).then(res => {
