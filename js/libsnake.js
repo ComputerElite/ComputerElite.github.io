858c4c0df4e8f61272e7a3caee104003e2cbf62a
ComputerElite <>
2021-08-09 19:13:16 +0200

    snake

---
+++ js/libsnake.js
@@ -0,0 +1,48 @@
+var a = []
+var important = [
+    "ArrowUp",
+    "ArrowUp",
+    "ArrowDown",
+    "ArrowDown",
+    "ArrowLeft",
+    "ArrowRight",
+    "ArrowLeft",
+    "ArrowRight",
+    "KeyB",
+    "KeyA",
+    "Enter"
+]
+function Process(keycode) {
+    a.push(keycode)
+    if(a.length > 11) a.splice(0, 1)
+    if(arrayCompare(a, important))
+    {
+        console.log("loggers")
+        speed /= 1.3
+        Destroy()
+        Setup()
+        a = []
+    }
+}
+
+function arrayCompare(_arr1, _arr2) {
+    if (
+      !Array.isArray(_arr1)
+      || !Array.isArray(_arr2)
+      || _arr1.length !== _arr2.length
+      ) {
+        return false;
+      }
+    
+    // .concat() to not mutate arguments
+    const arr1 = _arr1.concat().sort();
+    const arr2 = _arr2.concat().sort();
+    
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) {
+            return false;
+         }
+    }
+    
+    return true;
+}
