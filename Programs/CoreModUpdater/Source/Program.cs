6bca7aac88962d53228e6b75d6b4997afe3682f6
ComputerElite <ComputerElite@CrashGods>
2023-03-09 13:10:38 +0100

    Add Core mods updater program

---
+++ Programs/CoreModUpdater/Source/Program.cs
@@ -0,0 +1,7 @@
+﻿using System.Net;
+
+string url = "https://git.bmbf.dev/unicorns/resources/-/raw/master/com.beatgames.beatsaber/core-mods.json";
+Console.WriteLine("Downloading core mods from " + url);
+WebClient c = new ();
+c.DownloadFile(url, "coreMods.json");
+Console.WriteLine("done");
