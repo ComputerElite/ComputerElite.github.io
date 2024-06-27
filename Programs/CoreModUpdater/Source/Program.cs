b900694697c601b56034e75f8cf38c2cf2f695d1
ComputerElite <71177995+ComputerElite@users.noreply.github.com>
2024-06-27 06:28:56 +0000

    fix link

--- Programs/CoreModUpdater/Source/Program.cs
+++ Programs/CoreModUpdater/Source/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System.Net;
 
-string url = "hhttps://raw.githubusercontent.com/QuestPackageManager/bs-coremods/main/core_mods.json";
+string url = "https://raw.githubusercontent.com/QuestPackageManager/bs-coremods/main/core_mods.json";
 Console.WriteLine("Downloading core mods from " + url);
 WebClient c = new ();
+c.Headers.Add ("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; " + 
+                                  "Windows NT 5.2; .NET CLR 1.0.3705;)");
 c.DownloadFile(url, "coreMods.json");
 Console.WriteLine("done");
